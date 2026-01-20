3b5379362455365c07bc8729fc97dba427c1e860
user <liza.kostina90@gmail.com>
2026-01-20 10:34:33 +0200

    update admin User

--- user/admin.py
+++ user/admin.py
@@ -5,10 +5,20 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    list_display = ('id', 'username', 'email', 'role', 'is_staff')
-    list_filter = ('role', 'is_staff')
-    search_fields = ('username', 'email')
+    list_display = ("id", "username", "telegram_id", "role", "is_staff")
+    list_display_links = ("id", "username")
+    list_filter = ("role", "is_staff")
+    search_fields = ("username", "email")
 
-    fieldsets = BaseUserAdmin.fieldsets + (
-        ('Additional info', {'fields': ('role', 'avatar')}),
+    fieldsets = (
+        (None, {
+            "fields": ("username", "telegram_id", "role", "is_staff"),
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "telegram_id", "password1", "password2", "role")
+        }),
     )
