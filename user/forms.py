59a0ff4f1d9c77061fd91a0d5d08f033966d533b
user <liza.kostina90@gmail.com>
2026-01-20 13:09:51 +0200

    create form for register page

---
+++ user/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+from .models import User
+
+
+class RegisterForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("username", "telegram_id", "role")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+
+        user.set_password(str(user.telegram_id))
+
+        if commit:
+            user.save()
+
+        return user
