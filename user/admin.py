22517aa0406e8f4b17a895b66fdb8a017c3af7c2
user <liza.kostina90@gmail.com>
2026-01-20 09:09:11 +0200

    Initial project setup

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('id', 'username', 'email', 'role', 'is_staff')
+    list_filter = ('role', 'is_staff')
+    search_fields = ('username', 'email')
+
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Additional info', {'fields': ('role', 'avatar')}),
+    )
