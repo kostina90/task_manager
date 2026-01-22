d624b5b45c708d2308d684a2ec418cf5e43c02de
user <liza.kostina90@gmail.com>
2026-01-22 08:04:09 +0200

    create create page for task

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import TaskListView, TaskDetailView
+from .views import TaskListView, TaskDetailView, TaskCreateView
 
 
 app_name = "tasks"
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="tasks_list"),
-    path("<int:pk>/", TaskDetailView.as_view(), name="task_detail")
+    path("<int:pk>/", TaskDetailView.as_view(), name="task_detail"),
+    path("create/", TaskCreateView.as_view(), name="task_create"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.db.models import Q
+from django.urls import reverse_lazy
 
 from .models import Task
 
@@ -27,3 +28,14 @@ class TaskDetailView(DetailView):
         return Task.objects.filter(
             Q(creator=user) | Q(executor=user)
         ).distinct()
+    
+
+class TaskCreateView(CreateView):
+    model = Task
+    template_name = "tasks/task_create.html"
+    fields = ("title", "description", "executor", "status", "deadline", "priority")
+    success_url = reverse_lazy("tasks:tasks_list")
+
+    def form_valid(self, form):
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
